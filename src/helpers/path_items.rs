6e09796bf96fe3751d0087f3e8e4e07411d30dfe
carmachado <carmachado.dev@gmail.com>
2022-12-11 14:20:58 -0300

    feat: add path_items

---
+++ src/helpers/path_items.rs
@@ -0,0 +1,66 @@
+pub struct PathItems {
+    pub from: String,
+    pub to: String,
+}
+
+impl PathItems {
+    pub fn new(from: &String, to: &String) -> Self {
+        Self {
+            from: from.clone(),
+            to: to.clone(),
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {
+            from: String::new(),
+            to: String::new(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.from.is_empty() && self.to.is_empty()
+    }
+}
+
+impl PathItems {
+    fn get_from(&self, file: &Self) -> String {
+        if !self.from.is_empty() {
+            return self.from.clone();
+        }
+
+        if self.to == file.from {
+            return file.to.clone();
+        }
+
+        file.from.clone()
+    }
+
+    fn get_to(&self, file: &Self) -> String {
+        if !self.to.is_empty() {
+            return self.to.clone();
+        }
+
+        if self.from == file.to {
+            return file.from.clone();
+        }
+
+        file.to.clone()
+    }
+
+    pub fn normalized(&self, file: &Self) -> Self {
+        Self {
+            from: self.get_from(file),
+            to: self.get_to(file),
+        }
+    }
+}
+
+impl Clone for PathItems {
+    fn clone(&self) -> Self {
+        Self {
+            from: self.from.clone(),
+            to: self.to.clone(),
+        }
+    }
+}
